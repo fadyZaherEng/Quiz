702ba70a4f49b22f7e409f4a8c0bf42c548956e2
fadyZaherEng <fady33zaher@gmail.com>
2023-07-15 16:28:43 +0300

    upload 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quizes/modules/result_screen/result_screen.dart';
+import 'package:quizes/modules/welcome/welcome_screen.dart';
+import 'package:quizes/util/bindings_app.dart';
+
 
 void main() {
-  runApp();
+  runApp(MyApp());
 }
 
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialBinding: BilndingsApp(),
+      title: 'Flutter Quiz App',
+      home:  WelcomeScreen(),
+      getPages: [
+        GetPage(name: WelcomeScreen.routeName, page: () => WelcomeScreen()),
+        GetPage(name: QuizScreen.routeName, page: () =>  QuizScreen()),
+        GetPage(name: ResultScreen.routeName, page: () =>  ResultScreen()),
+      ],
+    );
+  }
+}
